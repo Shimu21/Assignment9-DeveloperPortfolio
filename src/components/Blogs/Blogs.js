490bd6658d6d132b52ffb06781b859346f6a802d
shimu21ph <shimu21ph@gmail.com>
2022-04-05 22:22:01 +0600

    blogs designing

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,6 +4,27 @@ const Blogs = () => {
     return (
         <div>
             <h1 className='text-5xl font-bold text-center mt-8 mb-8'>Blogs of <span className='text-teal-700'>Exclusive Ladies Bags</span> </h1>
+            <div className='md:flex justify-between'>
+                <div>
+                    <h1>Question 1: What is Context API ? What is the purpose of it?</h1>
+                    <p>Answer: Context provides a way to pass data through the component
+                        tree without having to pass props manually at all level.
+                        It allows to share values between components without having to explicity
+                        pass a prop through every level of the tree.
+                    </p>
+                    <p>Purpose: The context API is a React structure that enables you to exchange unique details
+                        and assists in solving prop-drilling from all levels of your application.
+                    </p>
+                </div>
+                <div>
+                    <h1>Question 2: What is SEmantic Tag?</h1>
+                    <p>Answer: A Semantic element clearly describes its meaning to both the browser and the developer.
+                        It clearly describes their meaning in a human and machine readable way.
+                        Elements such as table, form, header, footer, etc. In programming, Semantics refers to the meaning of
+                        a piece of code.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MenuIcon } from '@heroicons/react/solid';
 
 const Header = () => {
     return (
-        <div className='flex justify-center py-4' style={{ backgroundColor: 'black', color: 'white' }}>
-            <nav>
-                <Link to='/home' className='mx-4'>HOME</Link>
-                <Link to='/review' className='mx-4'>REVIEW</Link>
-                <Link to='/dashboard' className='mx-4'>DASHBOARD</Link>
-                <Link to='/blogs' className='mx-4'>BLOGS</Link>
-                <Link to='/about' className='mx-4'>ABOUT</Link>
-            </nav>
+        <div className='flex justify-between py-4 bg-teal-900 text-white hover:bg-teal-300'>
+            <div className='w-6 h-6 justify-start'>
+                <MenuIcon></MenuIcon>
+            </div>
+            <div>
+                <nav className='md:flex justify-center'>
+                    <Link to='/home' className='mx-4'>HOME</Link>
+                    <Link to='/review' className='mx-4'>REVIEW</Link>
+                    <Link to='/dashboard' className='mx-4'>DASHBOARD</Link>
+                    <Link to='/blogs' className='mx-4'>BLOGS</Link>
+                    <Link to='/about' className='mx-4'>ABOUT</Link>
+                </nav>
+            </div>
         </div>
     );
 };
