0dd9e4a0d13159b95f1a3d9550519584642e09bd
shimu21ph <shimu21ph@gmail.com>
2022-04-05 22:52:20 +0600

    final touch

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,15 @@ const About = () => {
     return (
         <div>
             <h1 className='text-5xl font-bold text-center mt-8 mb-8'>Explore <span className='text-teal-700'>Us</span></h1>
-
+            <div className='justify-items-center'>
+                <h1 className='text-1xl font-bold text-center'>Buy most exclusive and fashionable ladies hand bags from our store.
+                    World's largest bag collections you will find here.
+                    Choose your favorite collections and order now !!!
+                </h1>
+                <div className='flex justify-center'>
+                    <button className='font-bold bg-teal-700 p-2 px-8 mt-16 mb-56 rounded hover:bg-sky-700'>Grab Now</button>
+                </div>
+            </div>
         </div>
     );
 };
